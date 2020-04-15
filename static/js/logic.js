a734d18365787501b5970e05593c963f3e879f35
Mercy Auma <kashasimiyu20@gmail.com>
2020-04-14 22:09:23 -0500

    added1

--- static/js/logic.js
+++ static/js/logic.js
@@ -25,20 +25,69 @@ d3.json(link, function(data) {
   L.geoJson(data).addTo(myMap);
 });
 
-// // Our style object
-// var mapStyle = {
-//   color: "white",
-//   fillColor: "pink",
-//   fillOpacity: 0.5,
-//   weight: 1.5
-// };
+// Load in geojson data
+var geoData = "/data/state";
 
-// // Grabbing our GeoJSON data..
-// d3.json(link, function(data) {
-//   // Creating a geoJSON layer with the retrieved data
-//   L.geoJson(data, {
-//     // Passing in our style object
-//     style: mapStyle
-//   }).addTo(map);
-// });
+var geojson;
+
+// Grab data with d3
+d3.json(geoData, function(data) {
+console.log(data);
+  // Create a new choropleth layer
+  geojson = L.choropleth(data, {
+
+    // Define what  property in the features to use
+    valueProperty: "Population",
+
+    // Set color scale
+    scale: ["#ffffb2", "#b10026"],
+
+    // Number of breaks in step range
+    steps: 10,
+
+    // q for quartile, e for equidistant, k for k-means
+    mode: "q",
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    },
+
+    // Binding a pop-up to each layer
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("population: " + feature.properties.Population + "<br>ICU beds:<br>" +
+        "$" + feature.properties.beds);
+    }
+  }).addTo(myMap);
+
+  // // Set up the legend
+  // var legend = L.control({ position: "bottomright" });
+  // legend.onAdd = function() {
+  //   var div = L.DomUtil.create("div", "info legend");
+  //   var limits = geojson.options.limits;
+  //   var colors = geojson.options.colors;
+  //   var labels = [];
+
+  //   // Add min & max
+  //   var legendInfo = "<h1>Median Income</h1>" +
+  //     "<div class=\"labels\">" +
+  //       "<div class=\"min\">" + limits[0] + "</div>" +
+  //       "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+  //     "</div>";
+
+  //   div.innerHTML = legendInfo;
+
+  //   limits.forEach(function(limit, index) {
+  //     labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+  //   });
+
+  //   div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  //   return div;
+  // };
+
+  // // Adding legend to the map
+  // legend.addTo(myMap);
+
+});
 
